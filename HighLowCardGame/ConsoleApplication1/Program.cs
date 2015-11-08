5267492bba13dd3242835e5cdd14449dea1e864c
กังฟู 602 <klaw.amz@hotmail.com>
2015-11-08 15:17:43 +0700

    570610590 LAB9

---
+++ HighLowCardGame/ConsoleApplication1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HighLowCardGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Control D = new Control();
+            
+            Console.ReadLine();
+
+        }
+    }
+}
